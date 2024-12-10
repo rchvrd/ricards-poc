6ff63a3731cc9cf90628318f75b1ab0c5aaf8500
rchvrd <rklavins17@gmail.com>
2024-12-10 13:12:01 +0200

    Fix & Test

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Adjust the version as needed
+    }
+  }
+}
+
 provider "aws" {
-  region                      = "us-west-2"
-  access_key                  = "mock_access_key"
-  secret_access_key           = "mock_secret_key"
-  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  skip_requesting_account_id  = true
+  region                      = "us-west-2"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
   endpoints {
     s3 = "http://localhost:4566"
   }
@@ -12,7 +22,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "s3-example-bucket"
-  acl    = "private"
 
   tags = {
     Purpose     = "TestLocalStack"
