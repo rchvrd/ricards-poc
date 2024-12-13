23162c8bbd3a13d65f073fb99952cd9da4f3a71d
rchvrd <rklavins17@gmail.com>
2024-12-13 14:57:24 +0200

    First run with my comp act

--- main.tf
+++ main.tf
@@ -8,16 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  s3_force_path_style         = true
-  skip_requesting_account_id  = true
   region                      = "us-west-2"
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
 }
 
 resource "aws_s3_bucket" "example_bucket" {
